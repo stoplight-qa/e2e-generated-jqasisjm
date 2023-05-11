eb081c2e76d0157302eb41b746c7cde8ba493bd4
qa-moksi-4-txdz@example.com <qa-moksi-4-txdz@example.com>
2023-05-11 18:11:03 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16461.stoplight-dev.com/api/v1/projects/cHJqOjI0OQ/spectral.js?branch_slug=main&token=550d2dd1-64e2-446f-b59e-c6c4df5eb830";
+export default { extends: ruleset };
